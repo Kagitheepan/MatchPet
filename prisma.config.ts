8eed64b22923a38363a6138234234cbad616ccac
Kagialpha <steefkagi@gmail.com>
2026-04-13 15:55:06 +0200

    prisma config change

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,12 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+import { defineConfig } from '@prisma/config';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env['DATABASE_URL'],
+    url: process.env.DATABASE_URL,
   },
 });
