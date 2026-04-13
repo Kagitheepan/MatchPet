2a66a6f472f277fddafc81733664412ce0bbbf94
Kagialpha <steefkagi@gmail.com>
2026-04-13 20:32:58 +0200

    reset prisma migrations

--- prisma/migrations/20260327090547_update_animal_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Animal` ADD COLUMN `isHousetrained` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `isMixedBreed` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `isSpayedNeutered` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `isVaccinated` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `status` VARCHAR(191) NOT NULL DEFAULT 'Available';
